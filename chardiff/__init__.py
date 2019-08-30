f6b3e643ec2bb4e984210550b8e455f90a87d797
Matt Copperwaite <matt@copperwaite.net>
2019-08-30 15:52:12 +0100

    refactor

--- chardiff/__init__.py
+++ chardiff/__init__.py
@@ -1,22 +1,31 @@
 import colorama
 
+def marker_on(s, marked, marker):
+    if not marked:
+        s += marker
+        marked = True
+    return s, marked
+
+def marker_off(s, marked):
+    if marked:
+        s += colorama.Style.RESET_ALL
+        marked = False
+    return s, marked
 
 def chardiff(a, b, marker=colorama.Style.BRIGHT):
     s = ""
     marked = False
+
     for i in range(len(a)):
-        if i < len(b):
-            if a[i] != b[i]:
-                if not marked:
-                    s += marker
-            else:
-                marked = False
-                s += colorama.Style.RESET_ALL
+        if i > len(b)-1:
+            s, marked = marker_on(s, marked, marker)
         else:
-            s += marker
+            if a[i] == b[i]:
+                s, marked = marker_off(s, marked)
+            else:
+                s, marked = marker_on(s, marked, marker)
         s += a[i]
-    if marked:
-        s += colorama.Style.RESET_ALL
+    s, marked = marker_off(s, marked)
     return s
 
 
