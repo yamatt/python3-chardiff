2a88b59be79c2c70c3f4070b3609ee6c41b68635
Matt Copperwaite <matt@copperwaite.net>
2020-01-10 17:54:03 +0000

    adds a missing comma
    Missed a comma when copying content type from website

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='1.0',
     description='Compare 2 strings and highlight differences',
     long_description=open("README.md", encoding='utf-8').read(),
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     author='Matt Copperwaite',
     author_email='matt@copperwaite.net',
     url='https://github.com/yamatt/python3-chardiff',
